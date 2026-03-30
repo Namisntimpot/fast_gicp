44e25e941cd9544c5498ff8f175cbb08c6a46732
jiahengli <jiahengli25@stu.pku.edu.cn>
2026-03-30 16:12:56 +0000

    log

--- src/python/main.cpp
+++ src/python/main.cpp
@@ -675,13 +675,13 @@ PYBIND11_MODULE(pygicp, m) {
     				py::array(gicp.getSourceSize(), gicp.getSourceSqDistances().data()));
     })
     .def("set_source_covariances_fromqs", [] (FastGICP& gicp, py::array rotationsq, py::array scales){
-    	if(py::len(rotationsq)/4!=py::len(scales)/3){ std::cerr<<"qs size not matched" <<std::endl; return;}
+    	if(py::len(rotationsq)/4!=py::len(scales)/3){ std::cerr<<"[set_source_covariances_fromqs] qs size not matched" <<std::endl; return;}
     	const auto input_rotationsq = rotationsq.cast<std::vector<float>>();
     	const auto input_scales = scales.cast<std::vector<float>>();
     	gicp.setSourceCovariances(input_rotationsq, input_scales);
     })
     .def("set_target_covariances_fromqs", [] (FastGICP& gicp, py::array rotationsq, py::array scales){
-    	if(py::len(rotationsq)/4!=py::len(scales)/3){ std::cerr<<"qs size not matched" <<std::endl; return;}
+    	if(py::len(rotationsq)/4!=py::len(scales)/3){ std::cerr<<"[set_target_covariances_fromqs] qs size not matched" <<std::endl; return;}
     	const auto input_rotationsq = rotationsq.cast<std::vector<float>>();
     	const auto input_scales = scales.cast<std::vector<float>>();
     	gicp.setTargetCovariances(input_rotationsq, input_scales);
